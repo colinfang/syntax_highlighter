b22e7b91c84d44b016fda46dfdd1f79d79525da6
colinfang <colinfang@live.com>
2015-03-11 01:08:29 +0000

    Submit lexer choice in form instead of in args Get ready for shortcode

--- syntax_highlighter.py
+++ syntax_highlighter.py
@@ -5,11 +5,15 @@ from pygments import highlight
 from pygments.lexers import CSharpLexer, FSharpLexer, SLexer, PythonConsoleLexer, RConsoleLexer, PythonLexer, Python3Lexer, PostgresLexer, SqlLexer
 from pygments.formatters import HtmlFormatter
 from flask import Flask, render_template, request
+#from redis import StrictRedis
+
 app = Flask(__name__)
 
 LEXER_LIST = [CSharpLexer, FSharpLexer, SLexer, PythonConsoleLexer, RConsoleLexer, PythonLexer, Python3Lexer, PostgresLexer, SqlLexer]
 LEXER = {x.__name__: x for x in LEXER_LIST}
 
+DEFAULT_LEXER = 'CSharpLexer'
+DEFAULT_CODE = ''
 
 def format(code, lexer, style):
 	ret = highlight(code, LEXER[lexer](),
@@ -23,15 +27,23 @@ def format(code, lexer, style):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-	lexer = 'CSharpLexer'
-	code = ''
 	style = 'default'
 	formatted = ''
+	lexer = DEFAULT_LEXER
+	code = DEFAULT_CODE
 	if request.method == 'POST':
-		code = request.form['code']
-		lexer = request.args.get('lexer', '')
+		code = request.form.get('code', DEFAULT_CODE)
+		lexer = request.form.get('lexer', DEFAULT_LEXER)
+		if lexer not in LEXER:
+			lexer = DEFAULT_LEXER
+		shortcode = 'shortcode' in request.form
 		formatted = format(code, lexer, style)
-	return render_template('hello.html', name='wtfPage', lexer=lexer, formatted=formatted, code=code, validLexers=LEXER.keys())
+	elif request.method == 'GET':
+		pass
+		#lexer = request.args.get('lexer', DEFAULT_LEXER)
+		#if lexer not in LEXER:
+		#	lexer = DEFAULT_LEXER
+	return render_template('hello.html', name='wtfPage', lexer=lexer, formatted=formatted, code=code, validLexers=LEXER.keys(), shortcode='yes' if shortcode else '')
 
 if __name__ == '__main__':
 	app.run(debug=True)
